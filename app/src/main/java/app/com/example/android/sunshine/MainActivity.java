ec5513cd1bbbdedb9a0801f900b27ec367276d6d
Marina Borges <marina@3des.com.br>
2017-05-31 15:47:36 -0300

    Initial configurations (Lesson 2).

---
+++ app/src/main/java/app/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,45 @@
+package app.com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        //Inflate the menu, this adds items to the action bar if its present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        //Handle action bar item clicks here . The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        //as you specify a parent activity in Android Manifest.xml.
+        int id = item.getItemId();
+
+        //noinspection Simplifiable Statement
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+}
